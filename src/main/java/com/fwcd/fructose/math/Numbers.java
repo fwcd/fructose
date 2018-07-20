844ef34e1cb4adb00bde63f39074f96f1f15bfee
fwcd <fwcdmail@gmail.com>
2018-07-20 22:55:35 +0300

    Added convenience methods to create complex vectors and matrices from real numbers

--- src/main/java/com/fwcd/fructose/math/Numbers.java
+++ src/main/java/com/fwcd/fructose/math/Numbers.java
@@ -9,12 +9,14 @@ import java.util.stream.Collectors;
 public final class Numbers {
 	private Numbers() {}
 	
+	/** Creates a vector of real numbers. */
 	public static Vector<Real> realVector(double... values) {
 		return new Vector<>(Arrays.stream(values)
 				.mapToObj(Real::of)
 				.collect(Collectors.toList()));
 	}
 	
+	/** Creates a matrix of real numbers. */
 	public static Matrix<Real> realMatrix(double[][] values) {
 		return new Matrix<>(Arrays.stream(values)
 				.map(row -> Arrays.stream(row)
@@ -23,12 +25,30 @@ public final class Numbers {
 				.collect(Collectors.toList()));
 	}
 	
+	/** Creates a complex vector of real numebrs. */
+	public static Vector<Complex> complexVector(double... values) {
+		return new Vector<>(Arrays.stream(values)
+				.mapToObj(Complex::ofReal)
+				.collect(Collectors.toList()));
+	}
+	
+	/** Creates a complex vector. */
 	public static Vector<Complex> complexVector(double[][] values) {
 		return new Vector<>(Arrays.stream(values)
 				.map(cmplx -> Complex.of(cmplx[0], cmplx[1]))
 				.collect(Collectors.toList()));
 	}
 	
+	/** Creates a complex matrix of real numbers. */
+	public static Matrix<Complex> complexMatrix(double[][] reals) {
+		return new Matrix<>(Arrays.stream(reals)
+				.map(row -> Arrays.stream(row)
+					.mapToObj(Complex::ofReal)
+					.collect(Collectors.toList()))
+				.collect(Collectors.toList()));
+	}
+	
+	/** Creates a complex matrix. */
 	public static Matrix<Complex> complexMatrix(double[][][] values) {
 		return new Matrix<>(Arrays.stream(values)
 				.map(row -> Arrays.stream(row)
