194bc96b6ca10a77c4e8aa5804b2a20ab78b2bf8
fwcd <fwcdmail@gmail.com>
2019-01-12 17:01:42 +0100

    Added new removal methods to Closer

--- src/main/java/fwcd/fructose/Closer.java
+++ src/main/java/fwcd/fructose/Closer.java
@@ -1,8 +1,8 @@
 package fwcd.fructose;
 
-import java.io.Closeable;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * <p>An object that collects closeable/disposable
@@ -14,7 +14,7 @@ import java.util.Collection;
  * local variables, Java 8's {@code try-with-resources}
  * should be preferred.</p>
  */
-public class Closer implements AutoCloseable, Closeable {
+public class Closer implements AutoCloseable {
 	private final Collection<AutoCloseable> closeables = new ArrayList<>();
 	
 	public Closer(AutoCloseable... initialCloseables) {
@@ -39,6 +39,49 @@ public class Closer implements AutoCloseable, Closeable {
 		}
 	}
 	
+	/**
+	 * Removes, but does not close a closeable.
+	 */
+	public void forget(AutoCloseable closeable) {
+		closeables.remove(closeable);
+	}
+	
+	/**
+	 * Closes and removes a single closeable.
+	 * 
+	 * @throws CloserException - The {@code close()} throws an exception
+	 */
+	public void dispose(AutoCloseable closeable) {
+		closeables.remove(closeable);
+		try {
+			closeable.close();
+		} catch (Exception e) {
+			throw new CloserException(e);
+		}
+	}
+	
+	/**
+	 * Closes and removes closeables in this {@link Closer}.
+	 * 
+	 * @throws CloserException - The {@code close()} throws an exception
+	 */
+	public void disposeAll() {
+		try {
+			Iterator<AutoCloseable> iterator = closeables.iterator();
+			while (iterator.hasNext()) {
+				iterator.next().close();
+				iterator.remove();
+			}
+		} catch (Exception e) {
+			throw new CloserException(e);
+		}
+	}
+	
+	/**
+	 * Closes all closeables in this {@link Closer}.
+	 * 
+	 * @throws CloserException - The {@code close()} throws an exception
+	 */
 	@Override
 	public void close() {
 		try {
