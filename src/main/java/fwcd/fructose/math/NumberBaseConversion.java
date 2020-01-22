70bae78688109a408acca0d3b6b4e9727b14f32d
Savvas <32460265+savvasth96@users.noreply.github.com>
2020-01-23 00:25:29 +0200

    Update
    Supports decimal, binary, octal, hexadecimal.

--- src/main/java/fwcd/fructose/math/NumberBaseConversion.java
+++ src/main/java/fwcd/fructose/math/NumberBaseConversion.java
@@ -1,49 +1,36 @@
 package fwcd.fructose.math;
 
 /**
- * Converts a binary number to decimal vice versa 
+ * 
  * @author Savvas Theofilou
  */
 public class NumberBaseConversion {
-    
     /**
-     * This method converts a binary number to the corresponding decimal number
-     * @param binaryNum the number to be converted
-     * @return the corresponding decimal number
+     * Parses the number expressed in "from_base" base and returns its
+     * corresponsive in "to_base" base.
+     * @param number number to be converted
+     * @param from_base initial base
+     * @param to_base destination base
+     * @return the converted number in String, null for unsupported
      */
-    public static int convertBinaryToDecimal(int binaryNum){
-        int decimal = 0;  
-        int n = 0;  
-        while(true){  
-            if(binaryNum == 0){  
-                break;  
-            } 
-            else {  
-                int temp = binaryNum % 10;  
-                decimal += temp*Math.pow(2, n);  
-                binaryNum = binaryNum/10;  
-                n++;  
-            }      
+    public static String convert_base(String number,int from_base, int to_base) { 
+        if (from_base != 2 && from_base != 8 && from_base != 16
+                && from_base != 10 && to_base != 2 && to_base != 8
+                && to_base != 16 && to_base != 10){
+            System.out.println("Unsupported base/s");
+            return null;
         }
-        
-        return decimal; 
-    }
-    
-    /**
-     * This method converts a decimal number to the corresponding binary number
-     * @param decimalNum the number to be converted
-     * @return the corresponding binary number in a form of ArrayList
-     */
-    public static ArrayList<Integer> convertDecimalToBinary(int decimalNum){
-        ArrayList<Integer> binary = new ArrayList<>();
-        
-        int temp = 1;
-        while(decimalNum > 0){
-            binary.add(0, decimalNum % 2);
-            decimalNum /= 2;            
+        try {
+            if (to_base == 16){
+                return Integer.toString( Integer.parseInt(number, from_base),
+                to_base).toUpperCase();
+            }
+            return Integer.toString( Integer.parseInt(number, from_base),
+                to_base);
         }
-        
-        return binary;
-    }
-    
+        catch(NumberFormatException e){
+            System.out.println("Number Format Exception");
+            return null;
+        }
+    } 
 }
